732274f76ab4a567a82dab888388fe50b8b23058
Bruno <32314019+brunocedero@users.noreply.github.com>
2022-01-01 11:30:21 -0300

    codigo agregado

---
+++ read_excel.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+# Created by: Bruno Cedero
+# Read file and put the end date to employees from the list 
+
+require 'bundler/inline'
+require 'roo'
+require 'httparty'
+require 'uri'
+require 'date'
+require 'mongo'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'roo', '~> 2.8.0'
+  gem 'httparty'
+  gem 'byebug'
+end
+
+#file passed by console stored in ARGV(array),
+#in the first position
+file_path = ARGV[0]
+employees_file = Roo::Spreadsheet.open(file_path)
+
+# Create a hash, to acces columns by name
+headers={}
+employees_file.row(1).each_with_index {|header,i| headers[header] = i}
+
+rows = {}
+((employees_file.first_row + 1)..employees_file.last_row).each_with_index do |row, i|
+  @employee_id = employees_file.row(row)[headers['employee_id']]
+  @end_date = DateTime.now()
+  data = {}
+  data.store('employee_id', @employee_id)
+  data.store('end_date', @end_date)
+  rows[i] = data
+end
+
+token = 'token'
+base_uri = 'http://localhost:3001'
+
+#added end date by id
+rows.each do |employee_to_update|
+  employee_id = employee_to_update[1]['employee_id']
+  employee_end_date = employee_to_update[1]['end_date']
+
+  employee_data = {}
+  employee_data['employee_id'] = employee_id
+  employee_data['end_date'] = employee_end_date
+  endpoint = URI("#{base_uri}/api/v1/employee_hiring/#{employee_data['employee_id']}")
+  response = HTTParty.put(endpoint, body: employee_data.to_json, headers: { 'Content-Type' => 'application/json',
+    'Authorization' => 'Bearer ' + token })
+  
+    if response.code != 200
+      puts "ERROR --> #{response['error']} Operation aborted."
+    else
+      puts "Added end date"
+    end
+end
+
+
